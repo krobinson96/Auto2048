5f8c7ba454d1cf0bed587cb274b3ddc44d30e419
Kody Robinson <66851553+krobinson96@users.noreply.github.com>
2024-07-19 14:25:53 -0700

    Deprecated methods updated
    Selenium deprecated methods being used. Updates the deprecated methods. Also adds a flag so that each game is only ran once.

--- 2048.py
+++ 2048.py
@@ -1,21 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import ElementNotInteractableException
+from selenium.webdriver.common.by import By
 browser = webdriver.Firefox()
 browser.get('https://play2048.co/')
-htmlElem = browser.find_element_by_tag_name('html')
-while True:
+flag = True
+while flag:
     try:
-        htmlElem2 = browser.find_element_by_class_name('retry-button')
-        htmlElem = browser.find_element_by_class_name('score-container')
-        print('Your score was: ' + htmlElem.text)
-
-        htmlElem2.click()
+        htmlElem2 = browser.find_element(By.CLASS_NAME,'retry-button')
+        htmlElem = browser.find_element(By.CLASS_NAME,'score-container')
+        print('Your score was: ' + htmlElem.text)     
+        htmlElem2.click()        
+        flag = False
 
     except ElementNotInteractableException:
-        htmlElem = browser.find_element_by_tag_name('html')
+        htmlElem = browser.find_element(By.TAG_NAME,'html')
         htmlElem.send_keys(Keys.UP)
         htmlElem.send_keys(Keys.RIGHT)
         htmlElem.send_keys(Keys.DOWN)
         htmlElem.send_keys(Keys.LEFT)
-
